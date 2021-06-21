5676fa95d430cfd8698bc7c1aaa2a14814ad5f82
Anas <anaskhan.link@gmail.com>
2021-06-21 12:10:30 -0700

    updated the Ui

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FaFingerprint, FaBars } from "react-icons/fa";
 
 export default function Navbar() {
-  return <h1>Navbar</h1>;
+  return (
+    <div className="navbar">
+      <div className="navbar-container container">
+        <Link to="/" className="navbar-logo">
+          <FaFingerprint />
+          LEVISH
+        </Link>
+
+        {/* must be functional */}
+        <div className="menu-icon">
+          <FaBars />
+        </div>
+
+        <ul className="nav-menu active">
+          <li className="nav-items">
+            <Link to="/" className="nav-links">
+              Home
+            </Link>
+          </li>
+          <li className="nav-items">
+            <Link to="/" className="nav-links">
+              Service
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
